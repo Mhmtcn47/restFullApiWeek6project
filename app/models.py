1b791ed2215c9c36e870cd531930cc8763b43969
Hamed <104409876+Mhmtcn47@users.noreply.github.com>
2023-05-18 17:40:35 -0700

    i changed couple things

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from secrets import token_urlsafe
 
-from app import db, login
+from app import db,login
 
 @login.user_loader
 def load_user(user_id):
@@ -13,9 +13,9 @@ class User(db.Model):
     user_name = db.Column(db.String(60),unique=True)
     user_email =  db.Column(db.String(100),unique=True)
     user_password = db.Column(db.String(200))
-    token =  db.column(db.String(250),unique=True)
+    token =db.column(db.String(250),unique=True)
     date_created= db.Column(db.DateTime, default=datetime.utcnow)
-    character= db.relationship('Post', backref='author', lazy=True)
+    character= db.relationship('Post',backref='author', lazy=True)
 
     def __repr__(self):
         return f'User: {self.username}'
